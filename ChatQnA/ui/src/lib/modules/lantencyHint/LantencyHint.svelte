4db6ead7811bf6c24821d00f5c6f2a3730442429
Yue, Wenjiao <wenjiao.yue@intel.com>
2024-04-09 11:38:19 +0800

    modify ui

--- ChatQnA/ui/src/lib/modules/lantencyHint/LantencyHint.svelte
+++ ChatQnA/ui/src/lib/modules/lantencyHint/LantencyHint.svelte
@@ -63,11 +63,11 @@ class="relative mx-[2.8rem]  pt-[2.8rem] flex h-full w-[16.7%] flex-col items-ce
 			<p class={reductionSubtitleClass}>{cardContents.reductionSubtitle}</p>
 		</div>
 	{/if}
-	<!-- <img class="absolute bottom-[4.3rem] right-0 w-[3rem]" src={IntelFooterLogo} alt="Your SVG" /> -->
+	<!-- <img class="absolute bottom-[4.3rem] right-0 w-[4rem]" src={IntelFooterLogo} alt="Your SVG" /> -->
 	<!-- 6th -->
 	<img class="absolute bottom-[4.3rem] right-0 w-[4rem]" src={intelFooterLogoXeon} alt="Your SVG" />
 	<!-- footer -->
-	<!-- <img class="absolute bottom-[2.5rem] right-0 w-[3rem]" src={IntelFooterLogo} alt="Your SVG" /> -->
+	<!-- <img class="absolute bottom-[2.5rem] right-0 w-[4rem]" src={IntelFooterLogo} alt="Your SVG" /> -->
 
 
 </div>
